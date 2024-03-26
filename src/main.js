aaf554bce95e638aa92eee1b3fbb1f22b79fa8d6
Cheng Tan <reg_eax@outlook.com>
2024-03-26 22:27:01 +0800

    ElementPlus added

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import './assets/main.css'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
@@ -7,5 +9,6 @@ import router from './router'
 const app = createApp(App)
 
 app.use(router)
+app.use(ElementPlus)
 
 app.mount('#app')
