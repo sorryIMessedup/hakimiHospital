7b7738d2b60cd4bef0ded7ec0657b6a41eb398ef
Cheng Tan <reg_eax@outlook.com>
2024-04-13 16:14:23 +0800

    完善用户管理

--- src/page/admin/User.vue
+++ src/page/admin/User.vue
@@ -1,14 +1,15 @@
+<!-- eslint-disable vue/multi-word-component-names -->
 <template>
   <div class="hospital_user-container">
-    <el-table :data="userData" border style="width: 100%" >
+    <el-table :data="userData" border style="width: 100%">
       <el-table-column fixed label="用户名" width="100" prop="username" align="center"> </el-table-column>
       <el-table-column fixed label="权限" prop="auth" width="100" align="center"> </el-table-column>
       <el-table-column label="密保问题" prop="question" width="180" align="center"></el-table-column>
       <el-table-column label="密保答案" prop="answer" width="100" align="center"></el-table-column>
-      <el-table-column label="用户ID" prop="id" width="200" ></el-table-column>
+      <el-table-column label="用户ID" prop="id" width="150"></el-table-column>
       <el-table-column label="密码" prop="password" width="300"></el-table-column>
       <el-table-column fixed="right" width="150">
-        <template slot="header" slot-scope="scope">
+        <template slot="header">
           <el-input v-model="search" size="mini" placeholder="输入关键字搜索" />
         </template>
         <template slot-scope="scope">
@@ -19,17 +20,43 @@
     </el-table>
 
     <el-dialog :title="title" :visible.sync="dialogFormVisible">
-      <el-form :model="form">
-        <el-form-item label="用户名" :label-width="formLabelWidth">
-          <el-input v-model="form.username" autocomplete="off"></el-input>
+      <h1 style="padding-bottom: 20px; padding-left: 5px; margin-top: -20px;">请输入用户的相关信息</h1>
+      <el-form :rules="rules" label-position="left" label-width="80px" :model="formLabelAlign"
+        style="padding-left: 10px;">
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="formLabelAlign.username" placeholder="请输入用户名" :disabled="this.disabled[0]"></el-input>
         </el-form-item>
-        <el-form-item label="用户密码" :label-width="formLabelWidth">
-          <el-input v-model="form.password" autocomplete="off"></el-input>
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="formLabelAlign.password" placeholder="请输入密码" :disabled="this.disabled[1]"></el-input>
+        </el-form-item>
+        <el-form-item label="确认密码" prop="confirmedPassword">
+          <el-input v-model="formLabelAlign.confirmedPassword" placeholder="请输入确认密码"
+            :disabled="this.disabled[2]"></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱" prop="email">
+          <el-input v-model="formLabelAlign.email" placeholder="请输入邮箱" :disabled="this.disabled[3]"></el-input>
+        </el-form-item>
+        <el-form-item label="用户权限" prop="auth">
+          <el-select v-model="formLabelAlign.auth" placeholder="请选择权限" :disabled="this.disabled[4]">
+            <el-option label="管理员" value="2"></el-option>
+            <el-option label="实习生" value="1"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="密保问题" prop="question">
+          <el-select v-model="formLabelAlign.questions" placeholder="请选择密保问题" :disabled="this.disabled[5]">
+            <el-option label="你就读的学校是什么？" value="你就读的学校是什么？"></el-option>
+            <el-option label="你最印象深刻的人是谁？" value="你最印象深刻的人是谁？"></el-option>
+            <el-option label="你的宠物名字是什么？" value="你的宠物名字是什么？"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="密保答案" prop="answer">
+          <el-input v-model="formLabelAlign.answer" placeholder="请输入密保答案" :disabled="this.disabled[6]"></el-input>
         </el-form-item>
       </el-form>
-      <div slot="footer" class="dialog-footer">
-        <el-button @click="dialogFormVisible = false">取 消</el-button>
-        <el-button type="primary" @click="confirm">确 定</el-button>
+      <div class="hospital_register-button">
+        <div class="button" @click="confirm">注册</div>
+        &nbsp;&nbsp;
+        <div class="button" @click="dialogFormVisible = false">返回</div>
       </div>
     </el-dialog>
 
@@ -47,25 +74,34 @@
 <script>
 import { NetLoader } from "@/net";
 export default {
-  name: "User",
   data() {
     return {
       loader: new NetLoader("test"),
       search: "",
       userData: [],
-      form: {
-        username: "",
-        password: "",
-        id: "",
-        auth: "",
-        question: "",
-        answer: ""
-      },
       formLabelWidth: '120px',
       dialogTableVisible: false,
       dialogFormVisible: false,
       title: "修改用户信息",
-      status: "change"
+      status: "change",
+      formLabelAlign: {
+        username: '',
+        password: '',
+        confirmedPassword: '',
+        email: '',
+        questions: '',
+        answer: '',
+        auth: ''
+      },
+      rules: {
+        username: [{ required: true, message: '请输入用户名', trigger: 'blur' },
+        { min: 2, message: '长度至少为2个字符', trigger: 'blur' }],
+        password: [{ required: true, message: '请输入密码', trigger: 'blur' },
+        { min: 6, message: '长度至少为6个字符且包含字母', trigger: 'blur' }],
+        confirmedPassword: [{ required: true, message: '请输入确认密码', trigger: 'blur' }],
+        auth: [{ required: true, message: '请选择权限', trigger: 'blur' }]
+      },
+      disabled: [false, false, false, false, false, false, false]
     };
   },
   methods: {
@@ -83,7 +119,8 @@ export default {
             password: item.password,
             question: item.securityQuestion,
             answer: item.securityQuestionAnswer,
-            auth: item.auth
+            auth: item.auth,
+            email: item.email
           }
           this.userData.push(user);
         }
@@ -94,31 +131,32 @@ export default {
       this.title = "修改用户信息"
       this.status = "change"
       this.dialogFormVisible = true;
-      this.form.username = row.username;
-      this.form.password = row.password;
-      this.form.id = row.id;
+      this.formLabelAlign.username = row.username;
+      this.formLabelAlign.password = "";
+      this.formLabelAlign.email = row.email;
+      this.formLabelAlign.id = row.id;
+      this.formLabelAlign.questions = row.question;
+      this.formLabelAlign.answer = row.answer;
+      this.disabled = [true, false, false, true, false, true, true];
     },
 
     handleDelete(index, row) { // 删除用户
       console.log(index, row);
-      this.form.username = row.username;
-      this.form.password = row.password;
-      this.form.id = row.id;
+      this.formLabelAlign.username = row.username;
+      this.formLabelAlign.password = row.password;
+      this.formLabelAlign.id = row.id;
       this.$confirm("是否删除该用户？？", '提示', {
         confirmButtonText: '确定',
         cancelButtonText: '取消',
         type: 'warning'
       }).then(() => {
         this.loader.delete("/users/deleteUser/" + this.form.id, {}).then(val => {
+          console.log(val);
           this.$message({
             message: "删除成功",
             type: "success"
           })
-          this.loader.get("/users/findAllUsers").then(val => {
-            console.log(val);
-            this.getUsers();
-          })
-          window.location.reload();
+          this.getUsers();
         }, err => {
           console.log(err);
           this.$message.error("删除信息失败")
@@ -139,10 +177,9 @@ export default {
         type: 'warning'
       }).then(() => {
         if (this.status === "create") {
-          this.loader.post("/users/addUser", this.form).then(val => {
-            this.loader.get("/users/findAllUsers").then(val => {
-              this.getUsers();
-            })
+          this.loader.post("/users/addUser", this.formLabelAlign).then(val => {
+            console.log(val);
+            this.getUsers();
             this.dialogFormVisible = false;
             this.$message({
               type: 'success',
@@ -150,14 +187,24 @@ export default {
             });
           }, err => {
             this.dialogFormVisible = false;
-            this.$message.error("该用户已经存在，无法重复创建！！！")
+            this.$message.error(err.response.data.message);
           })
         }
         else if (this.status === "change") {
           this.loader.post("/users/changePasswordById", {
-            userId: this.form.id,
-            newPassword: this.form.password
+            userId: this.formLabelAlign.id,
+            newPassword: this.formLabelAlign.password
           }).then(val => {
+            console.log(val);
+            this.loader.post("users/updateAuth", {
+              id: this.formLabelAlign.id,
+              auth: this.formLabelAlign.auth
+            }).then(val => {
+              console.log(val);
+            }, err => {
+              console.log(err);
+              this.$message.error(err.response.data.message);
+            })
             this.dialogFormVisible = false;
             this.$message({
               type: 'success',
@@ -165,13 +212,8 @@ export default {
             });
             this.getUsers();
           }, err => {
-            if (this.status === "change") {
-              this.dialogFormVisible = false;
-              this.$message.error("修改用户信息发生错误")
-            } else if (this.status === "create") {
-              this.dialogFormVisible = false;
-              this.$message.error("该用户名已经存在，无法重复创建！！！")
-            }
+            console.log(err);
+            this.$message.error(err.response.data.message);
           })
         }
       }).catch(() => {
@@ -182,12 +224,9 @@ export default {
       });
     },
     addUser() {
-      this.form = {
-        username: "",
-        password: "",
-        id: null
-      },
-        this.title = "创建新用户";
+      this.disabled = [false, false, false, false, false, false, false];
+      this.formLabelAlign = {};
+      this.title = "创建新用户";
       this.status = "create"
       this.dialogFormVisible = true;
     }
@@ -205,6 +244,7 @@ export default {
   background-position: center center;
   background-repeat: repeat;
   background-size: 100%;
+
   .hospital_user-addBtn {
     position: fixed;
     bottom: 30px;
@@ -216,7 +256,29 @@ export default {
       fill: #409EFF;
       // box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
     }
+  }
+}
 
+.el-input {
+  max-width: 40vw;
+}
+
+.hospital_register-button {
+  width: 100%;
+  margin-top: 20px;
+  display: flex;
+  justify-content: center;
+
+  .button {
+    width: 80%;
+    padding: 10px 0px;
+    background: rgba(40, 40, 40);
+    color: #fff;
+    font-size: 14px;
+    text-align: center;
+    border-radius: 10px;
+    cursor: pointer;
+    margin-top: 20px;
   }
 }
 </style>
