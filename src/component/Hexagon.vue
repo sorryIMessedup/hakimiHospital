ab06298d4e5c0478f4c52b8b15799e5237c4f304
Cheng Tan <reg_eax@outlook.com>
2024-04-13 23:41:02 +0800

    format

--- src/component/Hexagon.vue
+++ src/component/Hexagon.vue
@@ -1,50 +1,54 @@
 <template>
-    <div class="hex-container">
-        <svg :width="slideLen" :height="slideLen" viewBox = '0 0 115.44 100' :style="{left: x + 'px', top: y + 'px'}">
-            <polygon points="28.86,0 86.58,0 115.44,50 86.58,100 28.86,100 0 50 28.86,0"
-                style="stroke:#fff;stroke-width:2; fill: transparent" />
-            <text x="28.86" y="55"><slot></slot></text>
-        </svg>
-    </div>
+	<div class="hex-container">
+		<svg :width="slideLen" :height="slideLen" viewBox='0 0 115.44 100' :style="{ left: x + 'px', top: y + 'px' }">
+			<polygon points="28.86,0 86.58,0 115.44,50 86.58,100 28.86,100 0 50 28.86,0"
+				style="stroke:#fff;stroke-width:2; fill: transparent" />
+			<text x="28.86" y="55">
+				<slot></slot>
+			</text>
+		</svg>
+	</div>
 </template>
 
 <script>
-    export default {
-        name: "Hexagon",
-        props: {
-            x: {
-                type: Number,
-                default: 0
-            },
-            y: {
-                type: Number,
-                default: 0
-            },
-            slideLen: {
-                type: Number,
-                default: 100
-            },
-        },
-        data() {
-            return {
+export default {
+	// eslint-disable-next-line vue/multi-word-component-names
+	name: "Hexagon",
+	props: {
+		x: {
+			type: Number,
+			default: 0
+		},
+		y: {
+			type: Number,
+			default: 0
+		},
+		slideLen: {
+			type: Number,
+			default: 100
+		},
+	},
+	data() {
+		return {
 
-            }
-        },
-        methods:{
+		}
+	},
+	methods: {
 
-        },
-        mounted() {
-            console.log(this.$slots.default)
-        }
-    }
+	},
+	mounted() {
+		console.log(this.$slots.default)
+	}
+}
 </script>
 
 <style lang="less" scoped>
 .hex-container {
-    cursor: pointer;
-    svg {
-        position: absolute;
-        cursor: pointer;
-    }
+	cursor: pointer;
+
+	svg {
+		position: absolute;
+		cursor: pointer;
+	}
 }
 </style>
