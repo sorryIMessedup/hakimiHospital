f27b5a976cbbe62e2595c5d06467240492ef288c
Cheng Tan <reg_eax@outlook.com>
2024-04-15 18:06:33 +0800

    添加Quill

--- src/main.js
+++ src/main.js
@@ -5,13 +5,18 @@ import {store} from "@/store"
 import "@/style/global.less"
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-import axios from 'axios'
+import VueQuillEditor from 'vue-quill-editor'
+
+import 'quill/dist/quill.core.css' // 引入样式
+import 'quill/dist/quill.snow.css' // snow theme
+import 'quill/dist/quill.bubble.css' // bubble theme
 // import '@/style/tailwindcss.css'
 
 Vue.config.productionTip = false
 // axios.defaults.withCredentials = true
 
 Vue.use(ElementUI);
+Vue.use(VueQuillEditor);
 new Vue({
   components: {
     App
