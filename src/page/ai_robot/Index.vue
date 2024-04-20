14ce9d37c3c24a34af0541db72f7482757c61504
ZenXingyhun <134491420+ZenXingyhun@users.noreply.github.com>
2024-04-20 21:20:26 +0800

    主页挤牙膏

--- src/page/ai_robot/Index.vue
+++ src/page/ai_robot/Index.vue
@@ -3,20 +3,26 @@
 </script>
 
 <template>
+  <div>
   <div class="jumbotron jumbotron-lg jumbotron-fluid mb-0 pb-3 bg-primary position-relative">
     <div class="container-fluid text-white h-100">
       <div class="d-lg-flex align-items-center justify-content-between text-center pl-lg-5">
         <div class="col pt-4 pb-4">
           <h1 class="display-3">我们使用了FastGPT来回答您的相关问题 </h1>
           <a @click="$router.push('/home/airobot')" class="btn btn-lg btn-outline-white btn-round">我康康</a>
-            <br><br><br><br><br><br><br><br><br>
+          <br><br><br>
         </div>
 
 
       </div>
     </div>
-  </div>
 
+  </div>
+    <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 1440 126" style="enable-background:new 0 0 1440 126;" xml:space="preserve">
+  <path class="bg-primary" d="M685.6,87.2C418.7,136.1,170.2,116.1,0,96v-96h1440v96C1252.7,73.8,1010,26.6,685.6,87.2z"></path>
+</svg>
+    <br><br><br><br><br><br><br><br><br>
+  </div>
 </template>
 
 <style scoped lang="less">
